30f6afd5b7275f13a7a867bdcd1a6b6981de41ba
Joachim <joachimwallaert@hotmail.com>
2026-01-30 20:41:07 +0000

    Initial commit

---
+++ js/supabase-config.js
@@ -0,0 +1,4 @@
+// js/supabase-config.js
+var supabaseUrl = 'https://tkuoedecatzveolfoeff.supabase.co';
+var supabaseKey = 'sb_publishable_rusa1B6stDum6oPix9BtZg_hp-YE1zF';
+var _supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
