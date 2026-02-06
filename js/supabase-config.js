8a2bad8c8e2512f91200a42b72bcc08bc2f5383c
Joachim <joachimwallaert@hotmail.com>
2026-02-06 15:53:01 +0000

    Update Supabase key in configuration file

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -1,4 +1,4 @@
 // js/supabase-config.js
 var supabaseUrl = 'https://tkuoedecatzveolfoeff.supabase.co';
-var supabaseKey = 'sb_publishable_rusa1B6stDum6oPix9BtZg_hp-YE1zF';
+var supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRrdW9lZGVjYXR6dmVvbGZvZWZmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk3OTY0NzMsImV4cCI6MjA4NTM3MjQ3M30.1IrckgUoXbi4Jt4OexSSPEjChnDbAYLKyijlv2ZKsdQ';
 var _supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
